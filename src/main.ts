1d5d3aded4737ded185cfe9ee53c1c4ca09b87a8
thinhbuihong <thinhbuihong.ma@gmail.com>
2021-10-01 15:23:10 +0700

    managing app configuration

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-const cookieSession = require('cookie-session');
+// const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['asd'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      //drop additional properties not in DTO
-      whitelist: true,
-    }),
-  );
+  // app.use(
+  //   cookieSession({
+  //     keys: ['asd'],
+  //   }),
+  // );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     //drop additional properties not in DTO
+  //     whitelist: true,
+  //   }),
+  // );
   await app.listen(3000);
 }
 bootstrap();
