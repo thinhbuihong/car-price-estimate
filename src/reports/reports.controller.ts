6a7d24ca9062f455ee37314c42a95c81c25d0d4b
thinhbuihong <thinhbuihong.ma@gmail.com>
2021-10-02 16:45:13 +0700

    permisssion system

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
@@ -6,6 +15,9 @@ import { CurrentUser } from 'src/users/decorators/current-user.decorators';
 import { User } from 'src/users/users.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -17,4 +29,13 @@ export class ReportsController {
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
+
+  @Patch('/:id')
+  @UseGuards(AdminGuard)
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {}
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { UsersService } from './users.service';
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptors';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -14,10 +15,14 @@ import { CurrentUserInterceptor } from './interceptors/current-user.interceptors
     UsersService,
     AuthService,
     // CurrentUserInterceptor,
-    {
-      useClass: CurrentUserInterceptor,
-      provide: APP_INTERCEPTOR,
-    },
+    // {
+    //   useClass: CurrentUserInterceptor,
+    //   provide: APP_INTERCEPTOR,
+    // },
   ],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumre: MiddlewareConsumer) {
+    consumre.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
