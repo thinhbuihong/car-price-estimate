b89ccf0746092afc9eeae655242114ed2ab2d409
thinhbuihong <thinhbuihong.ma@gmail.com>
2021-10-02 21:16:32 +0700

    query builders with typeORM

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { Report } from './reports.entity';
 
 @Injectable()
@@ -24,5 +25,23 @@ export class ReportsService {
 
     report.approved = approved;
     return this.repo.save(report);
+  }
+
+  createEstimate(estimateDto: GetEstimateDto) {
+    return (
+      this.repo
+        .createQueryBuilder()
+        .select('AVG(price)', 'pricee')
+        .where('make = :make', { make: estimateDto.make })
+        // .andWhere('approved IS TRUE')
+        .andWhere('model = :model', { model: estimateDto.model })
+        .andWhere('lng - :lng BETWEEN -5 AND 5', { lng: estimateDto.lng })
+        .andWhere('lat - :lat BETWEEN -5 AND 5', { lat: estimateDto.lat })
+        .andWhere('year - :year BETWEEN -3 AND 3', { year: estimateDto.year })
+        .orderBy('ABS(mileage - :mileage)', 'DESC')
+        .setParameters({ mileage: estimateDto.mileage })
+        .limit(3)
+        .getRawOne()
+    );
   }
 }
