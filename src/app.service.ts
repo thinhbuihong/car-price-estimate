8756b354815e23464cc83f359d9ea89524433eec
John Doe <thinhbuihong.ma@gmail.com>
2021-11-10 09:40:59 +0700

    update readme + hello world

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `<P>POST /auth/signup - Body:{email,password}</p> 
+    <P>POST /auth/signin - Body:{email,password}</p>
+    <P>POST /auth/signout </p>
+    <P>GET /auth/whoami - get current user</p>
+    <P>POST /reports - Body:{make, model, year, mileage, lng, lat, price}</p>
+    <P>PATCH /reports/:id - Body:{approved}</p>
+    <P>GET /reports - Query: make, model, year, mileage, lng, lat, price</p>
+    `;
   }
 }
